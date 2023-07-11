9417ff5b3982e9a6322b7c6f3b00a307aad02d9a
uijong0729 <krocha0729@gmail.com>
2023-07-11 23:02:10 +0900

    testcode

--- app/src/main/java/com/example/subscribewebpage/logic/SwLogicTest.kt
+++ app/src/main/java/com/example/subscribewebpage/logic/SwLogicTest.kt
@@ -1,12 +1,22 @@
 package com.example.subscribewebpage.logic
 
+import android.util.Log
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import org.junit.Test
 
 class SwLogicTest {
 
+    // CIO : Coroutine based I/O
+    private val client: HttpClient = HttpClient(CIO);
+
     @Test
-    fun test() {
-        println("test")
+    suspend fun test() {
+        val url = "https://www.naver.com/"
+        val response: HttpResponse = client.get(url)
+        Log.d("Test", response.readText());
         assert(true)
     }
 }
