97d4700425974df2c773e59d9dee4392460ff0ee
uijong0729 <krocha0729@gmail.com>
2023-11-07 23:41:31 +0900

    jsoup 삭제

--- app/src/main/java/com/example/subscribewebpage/logic/SwUpdateWorker.kt
+++ app/src/main/java/com/example/subscribewebpage/logic/SwUpdateWorker.kt
@@ -5,15 +5,7 @@ import android.util.Log
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import com.example.subscribewebpage.SwThreadPool
-import com.example.subscribewebpage.common.AppNotification
 import com.example.subscribewebpage.common.Const
-import com.example.subscribewebpage.model.data.Transaction
-import com.example.subscribewebpage.model.data.WebInfoEntity
-import com.example.subscribewebpage.viewmodel.WebInfoViewModel
-import org.jsoup.Jsoup
-import org.jsoup.nodes.Document
-import org.jsoup.select.Elements
-import java.net.UnknownHostException
 import java.util.concurrent.Callable
 
 class SwUpdateWorker(appContext: Context, workerParams: WorkerParameters) : Worker(appContext, workerParams) {
@@ -21,75 +13,7 @@ class SwUpdateWorker(appContext: Context, workerParams: WorkerParameters) : Work
 
     override fun doWork(): Result {
 
-        var getResult : Result? = SwThreadPool.es.submit(Callable {
-            val dao = Transaction.getInstance(context)?.webInfoDao()
-            WebInfoViewModel.list = dao?.getAll()
-            Log.d(Const.DEBUG_TAG, "Start Update Worker. do work update size : ${WebInfoViewModel.list!!.size}")
-
-            WebInfoViewModel.list?.stream()?.forEach { webInfo ->
-                with(webInfo) {
-                    var doc: Document?
-                    var elements: Elements?
-                    //Jsoup
-                    try {
-                        // https://jsoup.org/
-                        doc = Jsoup.connect(this.url).get()
-                    } catch (e: UnknownHostException) {
-                        Log.d(
-                            Const.DEBUG_TAG,
-                            "UnknownHostException : ${e.localizedMessage}, id : ${webInfo.id}"
-                        )
-                        dao?.updateEnableRequest(Const.DISABLE, webInfo.id)
-                        return@with
-                    }
-                    val response = doc.html()
-
-                    if (response.length < 61 || webInfo.url!!.length < 9) {
-                        dao?.updateEnableRequest(Const.DISABLE, webInfo.id)
-                        return@with
-                    }
-
-                    // 스크립트 태그 삭제
-                    if (this.cssQuery.isBlank()) {
-                        elements = doc.body().allElements
-                    } else {
-                        elements = doc.body().select(this.cssQuery)
-                    }
-
-                    for(removeTarget in elements.tagName(Const.TAG_NAME_SCRIPT)){
-                        removeTarget.remove()
-                    }
-
-                    val sb = StringBuilder(response.length)
-                    if (this.tagAttr.isBlank()) {
-                        // 유저관점
-                        val regex = """(</[A-Za-z>]+)|(.+=.+)|\s\s\s|\n""".toRegex()
-                        sb.append(regex.replace(elements.html(), ""))
-                    } else {
-                        // 기술자 관점
-                        val elementList = elements.tagName(this.tagAttr)
-                        for (line in elementList) {
-                            sb.append(line.text())
-                        }
-                    }
-
-                    //val regex = """([!"#${'$'}%&'()*+,./:;<=>?@\^_`{|}~-])""".toRegex()
-                    var currentQueryResult = sb.toString()
-                    if (sb.toString().length > 8192) {
-                        currentQueryResult = currentQueryResult.substring(0, 8191)
-                    }
-
-                    //Log.d(Const.DATA_TAG, regex.replace(currentQueryResult, ""))
-                    Log.d(Const.DATA_TAG, currentQueryResult)
-                    setWebInfoHtml(this, currentQueryResult)
-
-                    // 更新
-                    dao?.updateByIdToPreviousHtml(this.currentHtml, this.id)
-                    if (this.interval!! < Const.MINIMUM_INTERVAL) {
-                        this.interval = Const.MINIMUM_INTERVAL
-                    }
-                }
-            }
+        val getResult : Result? = SwThreadPool.es.submit(Callable {
             Log.d(Const.DEBUG_TAG, "Exit SwUpdate Worker")
             return@Callable Result.success()
         }).get()
@@ -97,37 +21,7 @@ class SwUpdateWorker(appContext: Context, workerParams: WorkerParameters) : Work
         if (getResult == null){
             return Result.failure()
         }else{
-            WebInfoViewModel.list?.stream()?.forEach{ webInfo ->
-                with(webInfo) {
-                    // == : 값 비교
-                    // === : 참조 비교
-                    var isUpdated = false
-                    if (this.previousHtml != this.currentHtml) {
-                        if (currentHtml.indexOf(this.searchKeyword, 0) > 0) {
-                            isUpdated = true
-                        }
-                    }
-
-                    Log.d(Const.DEBUG_TAG, "Notification Status $isUpdated")
-
-                    if (isUpdated) {
-                        AppNotification.createNotification(
-                            context,
-                            this.id,
-                            title,
-                            Const.NOTICE_UPDATE
-                        )
-                    }
-                }
-            }
             return getResult
         }
-    }
-
-    private fun setWebInfoHtml(webInfo: WebInfoEntity, str: String) {
-        if (webInfo.previousHtml.isEmpty()) {
-            webInfo.previousHtml = str
-        }
-        webInfo.currentHtml = str
     }
 }
